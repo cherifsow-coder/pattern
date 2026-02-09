4edfe07f25b254ae267402549c07344ff21c082c
cherifsow-coder <sowcherifchamsidine@gmail.com>
2026-02-09 11:21:16 +0000

    premier commit

---
+++ pattern.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int j,i,k;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=5-i;j++){
+            printf("*");
+            for(k=1;k<=2*i-1;k++){
+                printf("*");
+            }
+        }
+    }
+return 0;}
